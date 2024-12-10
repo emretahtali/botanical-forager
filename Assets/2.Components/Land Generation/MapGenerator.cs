e44f592d420327dfadc8b01384474566d23ad1bd
emretahtali <149873315+emretahtali@users.noreply.github.com>
2024-12-10 03:07:03 +0300

    terrain mesh generation foundation

--- Assets/2.Components/Land Generation/MapGenerator.cs
+++ Assets/2.Components/Land Generation/MapGenerator.cs
@@ -1,20 +1,92 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class MapGenerator : MonoBehaviour
 {
-    public int mapWidth;
-    public int mapHeight;
-    public float noiseScale;
+    public enum DrawMode {NoiseMap, ColorMap, DrawMesh}
+    public DrawMode drawMode;
     
-    public bool autoUpdate;
+    [Space]
+    public int seed;
+    
+    const int mapChunkSize = 241;
+    [Range(0, 6)] public int levelOfDetail;
+    public float noiseScale;
 
+    public int octaves;
+    public float lacunarity;
+    [Range(0, 1)] public float persistence;
+    
+    public Vector2 offset;
+    
+    [Space]
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
+    
+    [Space]
+    public TerrainType[] regions;
+    
+    [Space]
+    public bool autoUpdate;
+    
     public void GenerateMap()
     {
-        var noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        var noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, noiseScale, seed, octaves, persistence, lacunarity, offset);
+
+        Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
+        for (int y = 0; y < mapChunkSize; y++)
+        {
+            for (int x = 0; x < mapChunkSize; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight > regions[i].height) continue;
+                    
+                    colorMap[y * mapChunkSize + x] = regions[i].color;
+                    break;
+                }
+            }
+        }
         
         MapDisplay mapDisplay = FindObjectOfType<MapDisplay>(); // TODO: later change to serialized field
-        mapDisplay.DrawNoiseMap(noiseMap);
+        
+        switch (drawMode)
+        {
+            case DrawMode.NoiseMap:
+                mapDisplay.meshRenderer.gameObject.SetActive(false);
+                mapDisplay.textureRenderer.gameObject.SetActive(true);
+                mapDisplay.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+                break;
+            case DrawMode.ColorMap:
+                mapDisplay.meshRenderer.gameObject.SetActive(false);
+                mapDisplay.textureRenderer.gameObject.SetActive(true);
+                mapDisplay.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapChunkSize, mapChunkSize));
+                break;
+            case DrawMode.DrawMesh:
+                mapDisplay.meshRenderer.gameObject.SetActive(true);
+                mapDisplay.textureRenderer.gameObject.SetActive(false);
+                mapDisplay.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail),
+                    TextureGenerator.TextureFromColorMap(colorMap, mapChunkSize, mapChunkSize));
+                break;
+        }
+            
+    }
+
+    void OnValidate()
+    {
+        if (lacunarity < 1) lacunarity = 1;
+        if (octaves < 0) octaves = 0;
+    }
+
+    [Serializable]
+    public struct TerrainType
+    {
+        public string name;
+        public float height;
+        public Color color;
     }
 }
